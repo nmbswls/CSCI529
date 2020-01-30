b23b12d07a999d9b2d0674f4e0f687aa1d4d2009
unknown <nmbswls@163.com>
2020-01-30 02:59:39 -0800

    add explore 2D

---
+++ Assets/_CS/GamePlay/WildExplore/Editor/WildObstacleEditor.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(WildObstacle))]
+[CanEditMultipleObjects]
+public class WildObstacleEditor : Editor
+{
+    // Start is called before the first frame update
+    void OnSceneGUI()
+    {
+        WildObstacle obstacle = (WildObstacle)target;
+        Handles.color = Color.green;
+        
+
+        //if (GUILayout.Button("Rest Area"))
+        //{
+        //    arraw.shieldAre += 1.0f;  //改变数值
+        //}
+        for(int i=0;i< obstacle.PointsList.Count; i++)
+        {
+            _DoBodyFreeMoveHandle(obstacle.PointsList[i], i);
+        }
+       
+        if (GUI.changed)
+        {
+            EditorUtility.SetDirty(target);
+
+        }
+
+        //画出弧线
+        //Handles.DrawWireArc(arraw.transform.position, arraw.transform.up, -arraw.transform.right, 180, arraw.shieldAre);
+        ////计算值
+        //arraw.shieldAre = Handles.ScaleValueHandle(arraw.shieldAre,
+        //arraw.transform.position + arraw.transform.forward * arraw.shieldAre,
+        //arraw.transform.rotation, 1, Handles.ConeCap, 1);
+    }
+
+    private void _DoBodyFreeMoveHandle(Vector3 vPos, int idx)
+    {
+        WildObstacle obstacle = (WildObstacle)target;
+        Vector3 vTransform = obstacle.transform.TransformPoint(vPos);
+
+
+        EditorGUI.BeginChangeCheck();
+        Handles.color = Color.green;
+        Handles.Label(vTransform + Vector3.up * 0.2f, "P"+idx);
+
+        
+        //NOTE: vBodyHandler will be the body size change difference
+        Vector3 vBodyHandler = Handles.FreeMoveHandle(vTransform, Quaternion.identity, 0.15f * HandleUtility.GetHandleSize(obstacle.transform.position), Vector3.zero, SphereCap) - vTransform;
+        //Vector3 vBodyHandler = Handles.FreeMoveHandle(vTransform, Quaternion.identity, 0.15f * HandleUtility.GetHandleSize(footHold.transform.position), Vector3.zero, EditorCompatibilityUtils.SphereCap) - vTransform;
+        vBodyHandler = obstacle.transform.InverseTransformVector(vBodyHandler);
+
+        if (EditorGUI.EndChangeCheck())
+        {
+            obstacle.PointsList[idx] += new Vector2(vBodyHandler.x, vBodyHandler.y);
+            EditorUtility.SetDirty(target);
+        }
+        
+    }
+
+    
+
+    public static void SphereCap(int controlID, Vector3 position, Quaternion rotation, float size, EventType eventType = EventType.Ignore)
+    {
+        Handles.SphereHandleCap(controlID, position, rotation, size, Event.current.type);
+    }
+}
---
+++ Assets/_CS/GamePlay/WildExplore/WildObstacle.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WildObstacle : MonoBehaviour
+{
+    // 世界
+    public List<Vector2> PointsList = new List<Vector2>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    [ContextMenu("Ajust")]
+    void Adjust()
+    {
+        for(int i = 0; i < PointsList.Count; i++)
+        {
+            PointsList[i] = AdjustPos(PointsList[i],1);
+        }
+        transform.localPosition = AdjustPos(transform.localPosition, 1);
+        InitCollider();
+    }
+
+    public static Vector2 AdjustPos(Vector2 pos, int digit)
+    {
+        float rate = 1;
+        for (int i = 0; i < digit; i++)
+        {
+            rate *= 10;
+        }
+        float x = (int)Mathf.Round(pos.x * rate) / rate;
+        float y = (int)Mathf.Round(pos.y * rate) / rate;
+
+        return new Vector2(x, y);
+    }
+
+    public List<Vector2> GetExpandedOuterInWorld(float colDis)
+    {
+        if (PointsList.Count < 3)
+        {
+            return new List<Vector2>(PointsList);
+        }
+        List<Vector2> ret = new List<Vector2>();
+        float L = colDis;
+        int numPoints = PointsList.Count;
+        for (int i = 0; i < PointsList.Count; i++)
+        {
+            Vector2 v1 = PointsList[(i + numPoints - 1) % numPoints] - PointsList[i];
+            Vector2 v2 = PointsList[(i + 1) % numPoints] - PointsList[i];
+            float sin = Vector3.Cross(v1.normalized, v2.normalized).magnitude;
+            float mo = L / sin;
+            Vector2 dir = -(v1.normalized + v2.normalized);
+            Vector2 diff = dir * mo;
+            Vector2 pInWorld = transform.TransformPoint(PointsList[i] + diff);
+            ret.Add(pInWorld);
+        }
+        return ret;
+    }
+
+
+    private void InitCollider()
+    {
+
+
+        if(PointsList.Count < 2)
+        {
+            Debug.Log("点太少了");
+            return;
+        }
+
+        //List<Vector2> ret = GetOuter();
+        List<Vector2> edgePoints = PointsList;
+
+        DestroyImmediate(gameObject.GetComponent<Collider2D>());
+        
+        EdgeCollider2D collider = gameObject.AddComponent<EdgeCollider2D>();
+        Vector2[] points = new Vector2[edgePoints.Count + 1];
+        for(int i=0;i< edgePoints.Count; i++)
+        {
+            points[i] = edgePoints[i];
+        }
+        points[edgePoints.Count] = edgePoints[0];
+
+        collider.points = points;
+        collider.usedByEffector = true;
+    }
+}
